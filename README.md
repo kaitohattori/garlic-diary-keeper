d16f1667e396d72086d1e371ab7c085f682845b0
Kaito Hattori <kaichan394@gmail.com>
2022-10-30 14:33:59 +0900

    リファクタリング

--- README.md
+++ README.md
@@ -10,14 +10,14 @@ Twitter：https://twitter.com/garlic_diary
 
 以下の環境変数をすべてセットする。
 
-https://drive.google.com/file/d/1r-ZddNKMy2QICH6lulFIyOdFvcNrqIwV/view
+https://docs.google.com/document/d/18vC2pyuCw_srKAKiEbNMrlvyWINouokWnNn_VD8uyq8/view
 
 |KEY|VALUE|
 |---|---|
-|API_KEY| API キー |
-|API_KEY_SECRET| API シークレット|
-|ACCESS_TOKEN| アクセストークン |
-|ACCESS_TOKEN_SECRET| アクセストークンシークレット |
+|TWITTER_API_KEY| API キー |
+|TWITTER_API_KEY_SECRET| API シークレット|
+|TWITTER_ACCESS_TOKEN| アクセストークン |
+|TWITTER_ACCESS_TOKEN_SECRET| アクセストークンシークレット |
 
 ### 環境構築
 
--- src/garlic_diary_keeper/settings.py
+++ src/garlic_diary_keeper/settings.py
@@ -5,10 +5,10 @@ from error import SettingsLoadEnvironmentVariableException
 class Settings(object):
     def __init__(self):
         self.captured_image_path = '/tmp/apps/garlic_diary_keeper/image.jpg'
-        self.api_key = self.getenv('API_KEY')
-        self.api_key_secret = self.getenv('API_KEY_SECRET')
-        self.access_token = self.getenv('ACCESS_TOKEN')
-        self.access_token_secret = self.getenv('ACCESS_TOKEN_SECRET')
+        self.twitter_api_key = self.getenv('TWITTER_API_KEY')
+        self.twitter_api_key_secret = self.getenv('TWITTER_API_KEY_SECRET')
+        self.twitter_access_token = self.getenv('TWITTER_ACCESS_TOKEN')
+        self.twitter_access_token_secret = self.getenv('TWITTER_ACCESS_TOKEN_SECRET')
 
     def getenv(self, key: str):
         if os.getenv(key):
