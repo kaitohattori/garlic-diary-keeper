b70828afb9809b591acecfe0be06e6311d7ef1a2
Kaito Hattori <kaichan394@gmail.com>
2022-12-10 13:33:47 +0900

    hashtagをツイートに追加

--- src/garlic_diary_keeper/ai_text_generator.py
+++ src/garlic_diary_keeper/ai_text_generator.py
@@ -1,18 +1,14 @@
 import random
 
 from infrastructure.openai_client import OpenAIClient
-from infrastructure.twitter_client import TwitterClient
 from settings import settings
 
 
 class AITextGenerator(object):
     def __init__(self):
         self.openai_client = OpenAIClient()
-        self.twitter_client = TwitterClient()
 
-    def generate(self) -> str:
-        # トレンドを取得する
-        trend_topic = self.twitter_client.get_trend_topic()
+    def generate(self, trend_topic: str) -> str:
         # 文章を生成するための書き出しをランダムに取り出す
         prompt_template = random.choice(settings.openai_prompt_format_list)
         prompt = prompt_template.format(trend_topic)
--- src/garlic_diary_keeper/infrastructure/model/tweet/tweet_text.py
+++ src/garlic_diary_keeper/infrastructure/model/tweet/tweet_text.py
@@ -2,7 +2,14 @@ from exception.exceptions import TweetBodyLengthException
 
 
 class TweetText(object):
-    def __init__(self, text):
-        if len(text) > 140:
-            raise TweetBodyLengthException
-        self.value = text
+    def __init__(self, text: str, hash_tag: str):
+        hashtag = f'\n{hash_tag}'
+        self.value = f'{TweetText.sliceEndOfReadingPoint(text, 140 - len(hashtag))}{hashtag}'
+
+    @staticmethod
+    def sliceEndOfReadingPoint(text: str, max_length: int):
+        try:
+            index = text[:max_length].rindex('。')
+            return text[:index+1]
+        except:
+            return text[:max_length]
